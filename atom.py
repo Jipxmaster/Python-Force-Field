b7eb2dff7648b5097331b28d89af43e513991c7c
Jipxmaster <johnoshirokarita@gmail.com>
2024-07-05 22:20:52 -0500

    Add files via upload

--- atom.py
+++ atom.py
@@ -1,12 +1,23 @@
 from draw import Draw                                   # También se utilizará la interfaz
 import mol                                              # Para la constante de avogadro
 import math                                             # Funciones trigonométricas
-import copy                                             # Copiar idéntico
+
+
+def angulo(dx, dy):
+    angle = 0
+    if dx < 0:  # Si distancia x es negativo
+        angle = math.atan(dy / dx)
+    elif dx > 0:  # Si distancia x es positivo
+        angle = math.atan(dy / dx) + math.pi
+    elif dx == 0 and dy < 0:  # Si distancia x = 0 y distancia y negativo
+        angle = math.pi / 2
+    elif dx == 0 and dy > 0:  # Si distancia x = 0 y distancia y positivo
+        angle = (3 * math.pi) / 2
+    return angle
 
 
 class Atom:
     def __init__(self, pos, m, element):
-        self.pos0 = copy.deepcopy(pos)
         self.pos = pos                                  # Posición inicial, estática (static) sta
         self.mol = m                                    # Masa molar
         self.element = element                          # Elemento
@@ -21,6 +32,7 @@ class Atom:
         self.w = 0                                      # Velocidad angular (ANgular Velocity)
 
         self.va = 0                                     # Velocidad actual
+        self.vrot = 0                                   # Velocidad de rotación de molécula
 
     def draw(self, screen):                             # Simplemente muestra la coordenada actual
         if self.bonded:                                 # Crear enlace si el valor bonded es verdadero
@@ -37,29 +49,32 @@ class Atom:
         self.d = math.sqrt((dx ** 2) + (dy ** 2))       # Distancia entre los dos átomos
 
         # Obtención de los ángulos
-        if dx < 0:                                      # Si distancia x es negativo
-            self.angle = math.atan(dy / dx)
-        elif dx > 0:                                    # Si distancia x es positivo
-            self.angle = math.atan(dy / dx) + mol.pi
-        elif dx == 0 and dy < 0:                        # Si distancia x = 0 y distancia y negativo
-            self.angle = mol.pi/2
-        elif dx == 0 and dy > 0:                        # Si distancia x = 0 y distancia y positivo
-            self.angle = (3 * mol.pi) / 2
+        self.angle = angulo(dx, dy)
 
         self.w = math.sqrt(spring/(self.mol/(mol.avg * 1000)))  # Obtención de velocidad angular
         self.a = (self.v0 / self.w)
 
-    def compute(self, time):
+    def compute(self, time, cm):
         if self.bonded:                                 # Solo funcionará si tiene los parámetros de enlace
+            # --- Posición de los átomos ---
             a = self.a * (10 ** 10)                     # Amplitud en angstroms
             w_mod = self.w / (10**15)                   # 1 femtosegundos por fotograma
             x = math.cos(self.angle) * (self.d - (math.sin(w_mod * time) * a)) + self.anchor[0]
             y = math.sin(self.angle) * (self.d - (math.sin(w_mod * time) * a)) + self.anchor[1]
             self.pos = [x, y]                           # Posición de los átomos en movimiento
-            v = - self.w * self.a * math.cos(w_mod * time)
+
+            # --- Velocidad del átomo ---
+            v = - self.w * self.a * math.cos(w_mod * time)  # Velocidad del átomo
             x = math.cos(self.angle) * v                # Velocidad descompuesta en X
             y = math.sin(self.angle) * v                # Velocidad descompuesta en Y
             self.va = [x, y]                            # Velocidad del átomo
+
+            # --- Velocidad tangencial
+            dx = cm[0] - self.pos[0]                    # Distancia de centro de masa al átomo
+            dy = cm[1] - self.pos[1]
+            angle2 = self.angle - angulo(dx, dy)        # Ángulo
+            self.vrot = ((math.sin(angle2) * v) / self.d) * (10 ** 10)      # Velocidad de rotación
+            self.vrot = self.vrot / (10**15)            # Ajuste a radianes por femtosegundos
 
     def move(self, pos):                                # Mover átomo. Solo se usará para mover el hidrógeno
         self.pos[0] += pos[0]
--- water.py
+++ water.py
@@ -1,6 +1,8 @@
 from atom import Atom
 from print import Print
 from mol import Simkit
+from draw import Draw
+import math
 
 
 def dec(n):
@@ -13,7 +15,9 @@ class Water:
         self.h_1 = Atom([0.7570, -0.5859], 1.008, "H")
         self.h_2 = Atom([-0.7570, -0.5859], 1.008, "H")
         self.o_1 = Atom([0, 0], 15.9994, "O")
-        self.va = []                                        # Es una herramienta sorpresa que nos ayudará más tarde
+        self.va = []                                        # Velocidad actual
+        self.cm = []                                        # Centro de masa
+        self.vang = 0
 
     def set_temperature(self, temp):                        # Establecer temperatura con resorte constante
         temp = Simkit.velocity(temp, 1.008)                 # Dar velocidad según temperatura
@@ -22,17 +26,37 @@ class Water:
         self.h_2.sprivean(312.6463, temp[1], self.o_1)
 
     def compute(self, time):                                # Calcular la posición de los átomos que tengan resorte
-        self.h_1.compute(time)
-        self.h_2.compute(time)
+        # --- Centro de masa ---
+        h1 = self.h_1.pos                                   # Posiciones de los átomos
+        h2 = self.h_2.pos
+        o1 = self.o_1.pos
+        mh, mo = self.h_1.mol, self.o_1.mol                 # Pesos de los átomos
+        a = (h1[0] * mh) + (h2[0] * mh) + (o1[0] * mo)
+        b = mh + mh + mo
+        x = a / b                                           # Centro de masa en X
+        a = (h1[1] * mh) + (h2[1] * mh) + (o1[1] * mo)
+        y = a / b                                           # Centro de masa en Y
+        self.cm = [x, y]                                    # Posición centro de masa
+
+        # --- Cálculo de siguiente posición ---
+        self.h_1.compute(time, self.cm)                     # Calcular posición de hidrógenos
+        self.h_2.compute(time, self.cm)
         x = (self.h_1.va[0] + self.h_2.va[0]) / 10**5       # Sumar velocidades en X (angstroms / fs)
         y = (self.h_1.va[1] + self.h_2.va[1]) / 10**5       # Sumar velocidades en Y (angstroms / fs)
         self.va = [x, y]
         self.o_1.move(self.va)                              # Mover molécula según velocidades
 
+        # --- Rotación ---
+        ang = (self.h_1.vrot + self.h_2.vrot) * 100         # Rotación exagerada por 100
+        self.vang = dec(ang)
+        self.h_1.angle += ang
+        self.h_2.angle += ang
+
     def draw(self, screen):                                 # Dibujar todos los átomos
         self.h_1.draw(screen)
         self.h_2.draw(screen)
         self.o_1.draw(screen)
+        Draw.atom(screen, self.cm, 10, "C")                 # Dibujar centro de masa de la molécula
 
     def info(self, screen):
         Print("H_1 x [m/s]: " + str(dec(self.h_1.va[0])), 0, screen)        # Velocidad hidrógeno 1
@@ -43,3 +67,5 @@ class Water:
 
         Print("X [Å/fs]: " + str(dec(self.va[0])), 6, screen)               # Velocidad total
         Print("Y [Å/fs]: " + str(dec(self.va[1])), 7, screen)
+
+        Print("W [rad/fs]: " + str(self.vang), 9, screen)                   # Velocidad angular
